cb83962276cb47c858bb02b265685ce9315d1c7e
ThungaRajasekhar <rajasekhar.t@cumulations.com>
2023-06-06 07:22:57 +0530

    Initial commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="text-4xl font-sans font-bold py-6 pb-8">
+      {/* Logo */}91Social
+    </div>
+  );
+}
+
+export default Header;
